ef9b883512353c6b58b14724906b6033e88dfd9f
Jumana-Begum-J <87908039+Jumana-Begum-J@users.noreply.github.com>
2021-07-24 21:47:38 +0530

    Delete airline-database directory

--- airline-database/admin_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION["auserid"]);
-   unset($_SESSION["apassword"]);
-
-   echo '<script type="text/javascript">alert("Logging out!");window.location=\'index2.html\';</script>';
-
-?>
--- airline-database/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION["username"]);
-   unset($_SESSION["password"]);
-
-   echo '<script type="text/javascript">alert("Logging out!");window.location=\'index2.html\';</script>';
-   
-?>
